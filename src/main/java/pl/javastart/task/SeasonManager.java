7841159433cd4f193436acd5f49e1450c2839c0d
Alina Burnos <burnosalina@gmail.com>
2023-03-20 07:59:21 +0100

    4 pory roku

--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -6,7 +6,26 @@ public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
+        String season = getSeason(scanner);
+        Season valueForSeason = Season.getValueForSeason(season);
+        printMonthsForSeason(valueForSeason);
+    }
 
+    private void printMonthsForSeason(Season valueForSeason) {
+        System.out.println("W tej porze roku są następujące miesiące:");
+        String[] months = valueForSeason.getMonths();
+        for (String month : months) {
+            System.out.println(month);
+        }
+    }
+
+    private String getSeason(Scanner scanner) {
+        System.out.println("Podaj porę roku:");
+        Season[] values = Season.values();
+        for (Season value : values) {
+            System.out.println(value.getNamePl());
+        }
+        return scanner.nextLine();
     }
 
 }
